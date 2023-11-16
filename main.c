45a43960d5a71bc99d526ba55a6afcd38f897f00
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2023-11-15 17:45:14 -0800

    task

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 
 /**
 * main - main function for the custom shell
@@ -7,45 +7,44 @@
 */
 int main(void)
 {
-	char *input_cmd = NULL;
+	char *cmd = NULL;
 
 	size_t cmd_len = 0;
 	ssize_t read;
-	char *cmd_args[MAX_NUM_ARGS];
+	char *argv[MAX_NUM_ARGS];
 
 	char *full_path;
 
 	while (1)
 	{
 		if (checker())
-			write(STDOUT_FILENO, SHELL_PROMPT, my_strlen(SHELL_PROMPT));
+			write(STDOUT_FILENO, PROMPT, my_strlen(PROMPT));
 
-		read = getline(&input_cmd, &cmd_len, stdin);
+		read = getline(&cmd, &cmd_len, stdin);
 		if (read == -1)
 		{
-			free(input_cmd);
+			free(cmd);
 			write(0, "\n", 1);
 			return (0);
 		}
-		if (input_cmd[read - 1] == '\n')
-			input_cmd[read - 1] = '\0';
+		if (cmd[read - 1] == '\n')
+			cmd[read - 1] = '\0';
 
-		if (my_strlen(input_cmd) == 0)
+		if (my_strlen(cmd) == 0)
 			continue;
 
-		parse_command(input_cmd, cmd_args);
-		handle_exit(cmd_args);
-		handle_env(cmd_args);
+		parse_command(cmd, argv);
+		handle_exit(argv);
+		handle_env(argv);
 
-		full_path = get_full_path(cmd_args);
+		full_path = get_full_path(argv);
 		if (full_path == NULL)
 			continue;
 
-		if (run_command(cmd_args, full_path))
+		if (run_command(argv, full_path))
 			return (1);
 
 		free(full_path);
 	}
 	return (0);
 }
-
